3707a0939bdbdb39e64ba2efffb965b6611518e0
ElisaIanda <elisaianda01@gmail.com>
2023-10-17 17:10:51 +0200

    Add api show

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -13,4 +13,10 @@ class ProjectController extends Controller
 
         return response()->json( $projects);
     }
+
+    public function show($id){
+        $project = Project::where("id", $id)->with("type", "technologies")->firstOrFail();
+
+        return response()->json($project);
+    }
 }
